c24e2a647c9a5e7126dcb5994da79239520cf952
Ido <idobenoun@gmail.com>
2024-12-15 23:37:47 +0200

    Skaffold added
    Deployment

--- query/index.js
+++ query/index.js
@@ -4,26 +4,28 @@ const cors = require("cors");
 const axios = require("axios");
 
 const app = express();
-app.use(bodyParser.json());
 app.use(cors());
+app.use(bodyParser.json());
 
 const posts = {};
 
 const handleEvent = (type, data) => {
-    
   if (type === "PostCreated") {
     const { id, title } = data;
+
     posts[id] = { id, title, comments: [] };
   }
 
   if (type === "CommentCreated") {
     const { id, content, postId, status } = data;
+
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
 
   if (type === "CommentUpdated") {
     const { id, content, postId, status } = data;
+
     const post = posts[postId];
     const comment = post.comments.find((comment) => {
       return comment.id === id;
@@ -47,13 +49,16 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4002, async () => {
-  console.log("Query listening on 4002");
+  console.log("Listening on 4002");
+  try {
+    const res = await axios.get("http://event-bus-srv:4005/events");
 
-  const res = await axios.get("http://localhost:4005/events");
+    for (let event of res.data) {
+      console.log("Processing event:", event.type);
 
-  for (let event of res.data) {
-    console.log("Processing event:", event.type);
-
-    handleEvent(event.type, event.data);
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log("Error:", error.message);
   }
 });
